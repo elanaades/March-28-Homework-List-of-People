f323bd36c6bc21971af0230a782cfe29c224bd4e
Elana Ades <elanaades1@gmail.com>
2023-03-29 12:31:14 -0400

    Delete Updated. Bug: If you delete all including the first row, it starts where it left off and doesn't add.

--- March 27 Homework List of People/wwwroot/js/People.js
+++ March 27 Homework List of People/wwwroot/js/People.js
@@ -8,7 +8,7 @@
     $("#add-rows").on('click', function () {
 
         index++;
-        let newPersonRow = 
+        let newPersonRow =
             `<div class="row person-row" style="margin-bottom: 10px;">
         <div class="col-md-4">
           <input class="form-control firstname-field" type="text" name="people[${index}].firstname" placeholder="First Name" />
@@ -16,14 +16,38 @@
         <div class="col-md-4">
           <input class="form-control lastname-field" type="text" name="people[${index}].lastname" placeholder="Last Name" />
         </div>
-        <div class="col-md-4">
+        <div class="col-md-3">
           <input class="form-control age-field" type="text" name="people[${index}].age" placeholder="Age" />
         </div>
-      </div>`;
+        <button type="button" class="btn-close" aria-label="Close"></button>
+         </div>`;
 
         $("#ppl-rows").append(newPersonRow);
         ensureFormValidity();
     });
+
+    $("#ppl-rows").on('click', '.btn-close', function () {
+        console.log("delete clicked");
+        $(this).closest('.person-row').remove();
+
+        renameRows();
+    });
+
+    function renameRows() {
+        let newIndex = 0;
+        $(".person-row").each(function () {
+            const firstName = $(this).find('.firstname-field');
+            const lastName = $(this).find('.lastname-field');
+            const age = $(this).find('.age-field');
+
+            firstName.attr('name', `people[${newIndex}].firstname`);
+            lastName.attr('name', `people[${newIndex}].lastname`);
+            age.attr('name', `people[${newIndex}].age`);
+
+            newIndex++;
+        });
+    }
+    
 
     const isValidNum = value => !Number.isNaN(Number(value));
 
@@ -39,3 +63,12 @@
         });
     }
 });
+
+
+        //$(".person-row").each(function () {
+        //    const firstName = $(this).find('.firstname-field').val();
+        //    const lastName = $(this).find('.lastname-field').val();
+        //    const age = $(this).find('.age-field').val();
+
+
+        //});
